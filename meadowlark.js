c5aa6158611b7ed09f9c20bd5e459025766b7936
jihyun <solarb0526@gmail.com>
2017-01-13 01:21:03 +0900

    add handlebars static middleware

--- meadowlark.js
+++ meadowlark.js
@@ -12,26 +12,24 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function (req, res) {
-    res.type('text/plain');
-    res.send('Meadowlark');
+    res.render('home');
 });
 
 app.get('/about', function (req, res) {
-    res.type('text/plain');
-    res.send('About Meadowlark Travel');
+    res.render('about');
 });
 
+app.use(express.static(__dirname + '/public'));
+
 app.use(function (req, res) {
-    res.type('text/plain');
     res.status(404);
-    res.send('404 - Not Found');
+    res.render('404');
 });
 
 app.use(function (err, req, res, next) {
     console.error(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 });
 
 app.listen(app.get('port'), function () {
